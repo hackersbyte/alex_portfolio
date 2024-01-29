3dbc56be74b3f2848918f9a91ca52b30a8e41f48
hackersbyte <alexmutonga3@gmail.com>
2024-01-29 14:36:35 +0000

    Add about section and Connect to Sanity

--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
+import { urlFor, client } from '../../client';
 import './About.scss';
 
-const about = () => {
-  return (
-    <div>about</div>
-  )
-}
+const About = () => {
+  const [abouts, setAbouts] = useState([]);
 
-export default about
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className='head-text'> I know that <span> Intuitive Software</span> <br /> means <span>Good Business</span></h2>
+
+      <div className='app__profiles'>
+        {abouts.map((about, index) => (
+          <motion.div
+          whileInView={{ opacity: 1}}
+          whileHover={{  scale: 1.1 }}
+          transition={{ duration: .5, type: 'tween' }}
+          className='app__profile-item'
+          key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className=' bold-text' style={{ marginTop: 20 }}> {about.title} </h2>
+            <p className='p-text' style={{ marginTop: 10}}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    
+    </>
+  );
+};
+
+export default About
+
+
